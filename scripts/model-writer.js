11b6cb70a219117ed19bbcafb94623016cc4e43a
shelbykauth <shelbykauth@gmail.com>
2017-03-07 21:07:40 -0500

    Added JS Form Getter
    Add ModelWriter namespace object
    Create a JavaScript form getter. This will allow us to update the fields
    now without having to update the code to the new fields later

--- scripts/model-writer.js
+++ scripts/model-writer.js
@@ -16,6 +16,30 @@ $(document).ready(function(){
         $(".args-container").append(ele);
     });
 });
+var ModelWriter = {};
+ModelWriter.readForm = function() {
+    var formArray = $("#form-model-writer").serialize().split("&");
+    var formObject = {};
+    var args = {};
+    for (var i in formArray) {
+        i = parseInt(i);
+        var name = formArray[i].match(/^[^=]*/)[0]; //regex finds everything before the =.
+        var value = formArray[i].match(/[^=]*$/)[0]; //regex finds everything after the =.
+        if (name == "arg_name%5B%5D") {
+            args[value] = {"type":formArray[i+1].match(/[^=]*$/)[0]};
+            i++;
+            continue;
+        }
+        formObject[name] = value;
+    }
+    var returnObject = {};
+    returnObject.language = formObject.language;
+    returnObject.company = formObject.company;
+    returnObject.model = formObject.namespace;
+    returnObject.namespace = formObject.model;
+    returnObject.args = args;
+    return returnObject;
+}
 
 function remove_model_property(ele){
     if ($(".args").length > 1){
