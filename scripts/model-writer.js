bc20f57156a279a8f51e161220ddbc414f081e8e
shelbykauth <shelbykauth@gmail.com>
2017-03-09 00:43:13 -0500

    Replace input with select for property types

--- scripts/model-writer.js
+++ scripts/model-writer.js
@@ -15,6 +15,18 @@ $(document).ready(function(){
         var ele = $(".args").first().clone();
         $(".args-container").append(ele);
     });
+    $("#language").on("change", function() {
+        switch ($(this).val()) {
+            case "php":
+                ModelWriter.defaultPropertyTypes = 
+                    ["int","bool","number","string","array","object"];
+            break;
+        }
+    }).trigger("change");
+    $("#types").on("change", function () {
+        $("input#add-type").val($(this).val());
+    });
+    ModelWriter.populatePropertyTypes();
 });
 var ModelWriter = {};
 function remove_model_property(ele){
@@ -23,6 +35,61 @@ function remove_model_property(ele){
     } else {
         modal_errors('You must have at least one model property!');
     }
+}
+
+ModelWriter.defaultPropertyTypes = ["int"
+                                   ,"bool"
+                                   ,"number"
+                                   ,"string"
+                                   ,"array"
+                                   ,"object"];
+                                   
+ModelWriter.customPropertyTypes = [];
+
+ModelWriter.addPropertyType = function() {
+    var cTypes = ModelWriter.customPropertyTypes;
+    var dTypes = ModelWriter.defaultPropertyTypes;
+    var name = $("input#add-type").val();
+    if (cTypes.indexOf(name) != -1 || dTypes.indexOf(name) != -1) {
+        modal_errors("This property type already exists!");
+        return;
+    }
+    cTypes.push(name);
+    ModelWriter.populatePropertyTypes();
+}
+
+ModelWriter.removePropertyType = function() {
+    var cTypes = ModelWriter.customPropertyTypes;
+    var dTypes = ModelWriter.defaultPropertyTypes;
+    var name = $("input#add-type").val();
+    if (dTypes.indexOf(name) != -1) {
+        modal_errors("Cannot remove default property type!");
+        return;
+    }
+    if (cTypes.indexOf(name) == -1) {
+        modal_errors("Cannot remove property type that does not exist.");
+        return;
+    }
+    cTypes.splice(cTypes.indexOf(name),1);
+    ModelWriter.populatePropertyTypes();
+}
+
+ModelWriter.populatePropertyTypes = function() {
+    var selects = $("select[name='types'], select[name='arg_type[]']");
+    var mainOption1 = "<option value='' disabled selected>Property Types</option>";
+    var otherOption1 = "<option value='' disabled selected>Property Type</option>";
+    var typeList = "";
+    for (var type of ModelWriter.defaultPropertyTypes) {
+        typeList += "<option value='"+type+"'>"+type+"</option>";
+    }
+    typeList += "<option value='' disabled>-------</option>";
+    for (var type of ModelWriter.customPropertyTypes) {
+        typeList += "<option value='"+type+"'>"+type+"</option>";
+    }
+    selects.empty();
+    $("select[name='types']").append(mainOption1);
+    $("select[name='arg_type[]']").append(otherOption1);
+    selects.append(typeList);
 }
 
 function modal_errors(html){
