1ced1701ec7136352d5e1e022df9e638d0115d1e
shelbykauth <shelbykauth@gmail.com>
2017-03-09 01:53:35 -0500

    added functions
    ModelWriter.ucfirst() to uppercase the first character
    ModelWriter.write() to parse the form data

--- scripts/model-writer.js
+++ scripts/model-writer.js
@@ -77,6 +77,82 @@ function remove_model_property(ele){
     }
 }
 
+ModelWriter.ucfirst = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+ModelWriter.write = function(isPlainText) {
+    var formData = ModelWriter.readForm();
+    var language = formData.language;
+    var company = formData.company;
+    var model = formData.model;
+    var namespace = formData.namespace;
+    var args = formData.args;
+    var output = "";
+    var nl = "<br>";
+    var sp = "&nbsp;";
+    var tab = sp + sp + sp + sp;
+    if (isPlainText) {
+        nl = "\n\r";
+        sp = " ";
+        tab = "    ";
+    }
+    switch (language) {
+        case "php":
+            output = "/*" + nl
+                   + " * (c) " + company + nl
+                   + " *" + nl
+                   + " * For the full copyright and license information, please view the LICENSE" + nl
+                   + " * file that was distributed with this source code." + nl
+                   + " */<br>" + nl
+                   + "namespace " + namespace + nl
+                   + nl
+                   + "/**" + nl
+                   + " * @author Marie Kauth <marie.kauth@gmail.com>" + nl
+                   + " */" + nl
+                   + "class " + model + nl
+		           + "{" + nl
+            for (arg in args) {
+                var type = args[arg].type;
+                output += nl
+                        + tab + "/** @param " + type + " $" + arg + "*/" + nl
+                        + tab + "private $" + arg + ";" + nl
+            }
+            for (arg in args) {
+                var type = args[arg].type;
+                output += nl
+                        + tab + "/**" + nl
+                        + tab + " * @return " + type + nl
+                        + tab + " */" + nl;
+                output += tab + "public function"
+                        + ((type === 'bool' || type === 'boolean') ? " is" : " get")
+                        + ModelWriter.ucfirst(arg) + "()" + nl;
+                output += tab + "{" + nl
+                        + tab + tab + "return $this->" + arg + ";" + nl
+                        + tab + "}" + nl
+                        + nl
+                        + tab + "/**" + nl
+                        + tab + " * @param " + type + " $" + arg + nl
+                        + tab + " */" + nl;
+                output += tab + "public function"
+                        + " set" 
+                        + ModelWriter.ucfirst(arg) + "($" + arg + ")" + nl;
+                output += tab + "{" + nl
+                        + tab + tab + "$this->" + arg + " = $" + arg + nl
+                        + tab + "}" + nl;
+            }
+            output += "}" + nl 
+                    + nl;
+        break;
+        case "js":
+        
+        break;
+        default:
+            output = "Language Not Available!";
+    }
+    return output;
+}
+
 ModelWriter.defaultPropertyTypes = ["bool"
                                    ,"boolean"
                                    ,"int"
